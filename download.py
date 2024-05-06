8600b950dac4ed240323f70a3ad5be3156a3418a
heinzo666 <166720679+heinzo666@users.noreply.github.com>
2024-05-06 20:46:19 +0700

    Update download.py

--- download.py
+++ download.py
@@ -2,19 +2,19 @@ import os
 from git import Repo
 import datetime as dt
 tgl = dt.datetime.now().strftime("%y%m%d_%H%M%S")
-os.chdir('../downloadadmin')
+os.chdir('../training')
 import shutil
 shutil.make_archive('HEINZO', 'zip', '/content/images')
-full_local_path = "/content/downloadadmin"
+full_local_path = "/content/training"
 
-backupname = "/content/downloadadmin/HEINZO.zip"
-backup = ('/content/downloadadmin/H' + str(tgl) + '.zip')
+backupname = "/content/training/HEINZO.zip"
+backup = ('/content/training/H' + str(tgl) + '.zip')
 os.rename(backupname, backup)
 
 
 repo = Repo(full_local_path)
 repo.git.add("-A")
-repo.index.commit("admin")
+repo.index.commit("fakes")
 
 #repo = Repo(full_local_path)
 origin = repo.remote(name="origin")
