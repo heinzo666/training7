76fcc4279387cff2e9fe8e636b64fca8fb79ba60
heinzo666 <166720679+heinzo666@users.noreply.github.com>
2024-05-06 20:42:52 +0700

    Add files via upload

---
+++ training.py
@@ -0,0 +1,23 @@
+import os
+from git import Repo
+import datetime as dt
+tgl = dt.datetime.now().strftime("%y%m%d_%H%M%S")
+os.chdir('../downloadadmin')
+#import shutil
+#shutil.make_archive('HEINZO', 'zip', '/content/images')
+full_local_path = "/content/downloadadmin"
+
+#backupname = "/content/training/HEINZO.zip"
+#backup = ('/content/training/H' + str(tgl) + '.zip')
+#os.rename(backupname, backup)
+
+
+repo = Repo(full_local_path)
+repo.git.add("-A")
+repo.index.commit("admin")
+
+#repo = Repo(full_local_path)
+origin = repo.remote(name="origin")
+origin.push()
+
+os.chdir('..')
